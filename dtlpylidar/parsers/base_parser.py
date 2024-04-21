839b4ff34309247453a9d2a3feda2013d999cb28
OfirDataloopAI <ofir.g@dataloop.ai>
2024-04-21 20:13:52 +0300

    fixed problem in images ext

--- dtlpylidar/parsers/base_parser.py
+++ dtlpylidar/parsers/base_parser.py
@@ -57,7 +57,8 @@ class LidarFileMappingParser(dl.BaseServiceRunner):
             for image_num, image_details in frame_images.items():
                 logger.info(f"Search image {image_num} for frame {frame_num}")
                 image_filepath = os.path.join(self.jsons_path, mapping_item.dir[1:], image_details.get("image_path"))
-                image_filepath = image_filepath.replace(".png", ".json")
+                image_ext = os.path.splitext(image_filepath)[1]
+                image_filepath = image_filepath.replace(image_ext, ".json")
                 with open(image_filepath, 'r') as f:
                     image_json = json.load(f)
 
@@ -141,7 +142,8 @@ class LidarFileMappingParser(dl.BaseServiceRunner):
 
         self.dataset = mapping_item.dataset
         uid = str(uuid.uuid4())
-        base_path = "{}_{}".format(self.dataset.name, uid)
+        base_dataset_name = self.dataset.name.replace(":", "-")
+        base_path = "{}_{}".format(base_dataset_name, uid)
         try:
             items_download_path = os.path.join(os.getcwd(), base_path)
             self.dataset.items.download(local_path=items_download_path,
