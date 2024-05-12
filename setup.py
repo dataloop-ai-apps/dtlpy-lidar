ff44f84aad8d1a08d9eb6a413de3d0639c7b7c1c
ShadiMah <shadi.m@dataloop.ai>
2024-05-12 15:03:42 +0300

    Changinf Readme filename in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md', encoding="utf8") as f:
+with open('README.MD', encoding="utf8") as f:
     readme = f.read()
 
 with open('requirements.txt') as f:
